67ddada4936148ac16590bbf7bffca37d48e7f68
clarabond <128716837+clarabond@users.noreply.github.com>
2024-09-11 07:51:49 -0300

    Update aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,8 @@
+const nomes = [ "Luiza", "Graziella", "Felipe", "Magali", "Maranhão", "Alexandre"];
+
 function aleaorio (lista){
     const posicao = Math.floor(Math.random()* lista.lenght;
     return lista[posicao];
 }
+
+export const nome = aleatorio (nomes)
